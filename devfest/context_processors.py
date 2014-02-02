d38913910fbf579b1a9caba2cc675b2e13c91639
Alex Dong <ihasnamespacestd@gmail.com>
2014-02-02 18:38:30 -0500

    improved structure of app

---
+++ devfest/context_processors.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.forms import AuthenticationForm
+
+def authentication_info(request):
+    '''
+    This context processor adds all the user info into the page that is needed for the
+    login form and signup form that appear in the header of every page, as well as other
+    basic information about the state of the current user.
+
+    This way the code is DRY-er: we don't need to copy these fields into every page handler.
+    '''
+    '''profile = Profile.get(request.user)
+    return {
+        'login_form': AuthenticationForm(),
+        'signup_form': CreateAccountForm(),
+        'username': request.user.username,
+        'is_authenticated': request.user.is_active and request.user.is_authenticated(),
+        'is_admin': profile.user.is_superuser if profile is not None else None
+    }'''
+
+    return {
+
+    }
---
+++ home/urls.py
@@ -0,0 +1,8 @@
+__author__ = 'matt'
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('',
+
+# None
+
+)
