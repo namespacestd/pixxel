367c54c82a77cc0b286bd15099c34326259c770a
Alex Dong <ihasnamespacestd@gmail.com>
2014-02-03 11:08:07 -0500

    changed to template structure and added some basic database objects

--- devfest/models.py
+++ devfest/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserAccount(models.Model):
+    user = models.ForeignKey(User)
+
+class GameInstance(models.Model):
+    users = models.ManyToManyField(UserAccount, related_name='game_instance_user')
+    current_judge = models.ForeignKey(UserAccount, related_name='game_instance_judge')
+
+class ScoreInstance(models.Model):
+    user = models.ForeignKey(UserAccount)
+    score = models.IntegerField(default=0)
+    game = models.ForeignKey(GameInstance)
