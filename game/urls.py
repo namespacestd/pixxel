b48ffd5a0c8a8c2e1f424220de609142daf1ca90
Alex Dong <ihasnamespacestd@gmail.com>
2014-02-03 19:00:40 -0500

    added room joining functionality

--- game/urls.py
+++ game/urls.py
@@ -3,5 +3,8 @@ from game import views
 
 urlpatterns = patterns('',
     url(r'^new_game/', views.new_game, name='new_game'),
-    url(r'^create_new_room', views.create_new_room, name='create_new_room')
+    url(r'^open_games/', views.open_games, name='open_games'),
+    url(r'^create_new_room', views.create_new_room, name='create_new_room'),
+    url(r'^room/(?P<room_name>\w+)/+$', views.game_room, name='game_room'),
+    url(r'^room/join_game/(?P<room_name>\w+)/+$', views.join_game, name='join_game')
 )
--- game/views.py
+++ game/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from devfest.models import GameInstance, UserAccount
 
 def new_game(request):
@@ -10,7 +10,7 @@ def create_new_room(request):
         
         if GameInstance.get(game_name):
             return render(request, 'game/game.html', { 'error' : 'already exists' })
-            
+
         new_game = GameInstance()
         new_game.game_room_name = game_name
         current_user = UserAccount.get(request.user)
@@ -20,3 +20,33 @@ def create_new_room(request):
 
     
     return render(request, 'game/game.html')
+
+def open_games(request):
+    open_games = GameInstance.objects.all()
+    return render(request, 'game/open_games.html', {
+        'open_games': open_games
+    })
+
+def game_room(request, room_name):
+    current_room = GameInstance.get(room_name)
+    userlist = current_room.users.all()
+    already_in_game = None
+
+    if request.user:
+        already_in_game = UserAccount.get(request.user) in userlist
+
+    return render(request, 'game/game_room.html', { 
+        'room_name' : room_name, 
+        'userlist' : current_room.users.all(),
+        'already_in_game' : already_in_game
+    })
+
+def join_game(request, room_name):
+    current_user = UserAccount.get(request.user)
+    current_room = GameInstance.get(room_name)
+    already_in_game = UserAccount.get(request.user) in current_room.users.all()
+
+    if not already_in_game:
+        current_room.users.add(current_user)
+
+    return HttpResponseRedirect('/game/room/'+room_name)    
