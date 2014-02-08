1d1f24b434b899f19330de5db65c12ccae43bca6
Alex Dong <ihasnamespacestd@gmail.com>
2014-02-08 06:09:13 -0500

    alsdkf

--- devfest/settings.py
+++ devfest/settings.py
@@ -218,3 +218,12 @@ LOGGING = {
         },
     }
 }
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+import os
+if os.environ.has_key('DATABASE_URL'):
+    DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
